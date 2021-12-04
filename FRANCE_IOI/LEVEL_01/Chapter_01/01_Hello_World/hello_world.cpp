3e8ed3689f7daa420be558877edc8ac547a90a6a
Ayckinn <ayckinn@pm.me>
2021-12-04 22:59:49 +0100

    Add FRANCE-IOI folder and first exercice

---
+++ FRANCE_IOI/LEVEL_01/Chapter_01/01_Hello_World/hello_world.cpp
@@ -0,0 +1,20 @@
+/* ============================== DESCRIPTION ============================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2020
+    LANGUAGE  : C++
+    LEVEL	  : 01
+    CHAPTER	  : 01
+    EXERCICE  : 01 - HELLO WORLD !
+
+   ========================== WHAT THE PROGRAM DOES ========================
+    Écrivez un programme permettant au robot d'afficher le texte : Hello world!
+
+ * ========================================================================= */
+
+#include <iostream>
+
+int main()
+{
+  	std::cout << "Hello world!\n";
+  	return 0;
+}
