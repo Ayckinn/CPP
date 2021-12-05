1b8c5662ceb0418e1f8eccecc414caa24da32772
Ayckinn <ayckinn@pm.me>
2021-12-05 11:41:03 +0100

    Add MATHEMATIQUES DE BASE folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_02/02_maths_base/maths_base.cpp
@@ -0,0 +1,22 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 02
+    EXERCICE  : 02 - MATHEMATIQUES DE BASE
+ * =================================================== */
+
+#include <iostream>
+
+#define repeat(nb) for (int _loop = 1, _max = (nb); _loop <= _max; _loop++)
+
+using namespace std;
+
+int main()
+{
+    repeat(13)
+    {
+        cout << "9 * 8 = 72" << endl;
+    }
+}
