572ba6b4698f411d1f1089e9371a595e9907a6d2
Ayckinn <ayckinn@pm.me>
2021-12-05 16:55:17 +0100

    Add DECOLLAGE FUSEE folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_03/08_decollage_fusee/decollage_fusee.cpp
@@ -0,0 +1,18 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 03
+    EXERCICE  : 08 - DECOLLAGE FUSEE
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+   for(int i = 100; i >= 0; i--)
+      std::cout << i << std::endl;
+
+   std::cout << "Décollage !" << std::endl;
+   return 0;
+}
