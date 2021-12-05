6d067eccd906014bd10e2463bef15d67e497786d
Ayckinn <ayckinn@pm.me>
2021-12-05 18:07:11 +0100

    Add AGE PETITS-ENFANTS folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_04/03_petits-enfants/age_p-enfants.cpp
@@ -0,0 +1,22 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 04
+    EXERCICE  : 03 - AGE DES PETITS-ENFANTS
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+   int ageCadet;
+   int ageAine;
+   std::cin >> ageCadet;
+   std::cin >> ageAine;
+   
+   int difference = ageAine - ageCadet;
+   std::cout << difference << std::endl;
+
+   return 0;
+}
