d6c6bafc6b385f0fef47590cf36d8ba39bf5a26b
Ayckinn <ayckinn@pm.me>
2021-12-05 11:31:57 +0100

    Add PUNITION folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_02/01_punition/punition.cpp
@@ -0,0 +1,19 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 02
+    EXERCICE  : 01 - PUNITION
+ * =================================================== */
+
+#include <iostream>
+#define loop(repeat) for(int i = 0; i < 135; i++)
+
+int main()
+{
+    loop(135)
+        std::cout << "Je dois respecter le Grand Sorcier.\n";
+
+    return 0;
+}
