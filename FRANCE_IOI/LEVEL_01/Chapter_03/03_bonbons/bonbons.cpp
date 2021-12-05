688e3a9dd3af5c6e4102cc7b5eaaba8926d83262
Ayckinn <ayckinn@pm.me>
2021-12-05 16:06:03 +0100

    Add BONBONS folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_03/03_bonbons/bonbons.cpp
@@ -0,0 +1,15 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 03
+    EXERCICE  : 03 - BONBONS POUR TOUT LE MONDE
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+   std::cout << ((25 + 30 + 27 + 22 - 8) *3 ) << std::endl;
+   return 0;
+}
