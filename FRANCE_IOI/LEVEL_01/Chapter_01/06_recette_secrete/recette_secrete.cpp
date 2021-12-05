0496d3cac6f2723e9098a53cd6aa22cc281ba16f
Ayckinn <ayckinn@pm.me>
2021-12-05 11:18:31 +0100

    Add RECETTE SECRETE folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_01/06_recette_secrete/recette_secrete.cpp
@@ -0,0 +1,23 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 01
+    EXERCICE  : 06 - RECETTE SECRETE
+ * =================================================== */
+
+#include <iostream>
+#include "robot.h"
+
+int main()
+{
+  remplir(5);
+  transferer(5, 3);
+  vider(3);
+  transferer(5, 3);
+  remplir(5);
+  transferer(5, 3);
+
+  return 0;
+}
