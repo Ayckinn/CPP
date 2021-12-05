b9a8f9784ab8e9e1916f576b3f4570edca5d77a6
Ayckinn <ayckinn@pm.me>
2021-12-05 17:50:09 +0100

    Add RECOLTES folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_04/01_Recoltes/recoltes.cpp
@@ -0,0 +1,20 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 04
+    EXERCICE  : 01 - RECOLTES
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+    int longueur_cote(0);
+    int masse(23);
+
+    std::cin >> longueur_cote;
+    std::cout << (masse * (longueur_cote * longueur_cote)) << std::endl;
+
+    return 0;
+}
