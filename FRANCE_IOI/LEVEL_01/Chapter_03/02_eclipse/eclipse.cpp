6011303cf101f5ac69b6f9075ca247b29924c6cb
Ayckinn <ayckinn@pm.me>
2021-12-05 16:00:11 +0100

    Add ECLIPSE folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_03/02_eclipse/eclipse.cpp
@@ -0,0 +1,16 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 03
+    EXERCICE  : 02 - L'ECLIPSE
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+   int eclipse(12581 - 11937);
+   std::cout << eclipse << std::endl;
+   return 0;
+}
