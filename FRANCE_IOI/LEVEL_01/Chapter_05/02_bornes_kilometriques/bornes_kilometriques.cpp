7f7267d87d6a4493de91e60b6d524ba5999d6b8c
Ayckinn <ayckinn@pm.me>
2021-12-06 21:28:16 +0100

    Add BORNES KILOMETRIQUES folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_05/02_bornes_kilometriques/bornes_kilometriques.cpp
@@ -0,0 +1,24 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 05
+    EXERCICE  : 02 - BORNES KILOMETRIQUES
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+    int borneA(0);
+    int borneB(0);
+
+    std::cin >> borneA >> borneB;
+
+    if(borneA < borneB)
+        std::cout << borneB - borneA << std::endl;
+    else
+        std::cout << borneA - borneB << std::endl;
+
+    return 0;
+}
