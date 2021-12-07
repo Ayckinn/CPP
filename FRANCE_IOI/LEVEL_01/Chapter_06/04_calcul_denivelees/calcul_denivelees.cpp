160958c604b81ccd6a73209c5bfa1e711ec1b4e1
Ayckinn <ayckinn@pm.me>
2021-12-07 22:24:21 +0100

    Add CALCUL DES DENIVELEES folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_06/04_calcul_denivelees/calcul_denivelees.cpp
@@ -0,0 +1,35 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 06
+    EXERCICE  : 04 - CALCUL DES DENIVELEES
+ * =================================================== */
+#include <iostream>
+#define LOOP(repeat) for(int i = 0; i < repeat; i++)
+
+int main()
+{
+   int montees_descentes(0);
+   int variations(0);
+   int montees(0);
+   int descentes(0);
+
+   std::cin >> montees_descentes;
+
+   LOOP(montees_descentes)
+   {
+      std::cin >> variations;
+
+      if(variations < 0)
+         descentes = descentes + variations;
+      else
+         montees = montees + variations;
+   }
+   if(descentes < 0)
+      descentes = -descentes;
+
+   std::cout << montees << std::endl << descentes << std::endl;
+   return 0;
+}
