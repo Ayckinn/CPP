78daedb3e0b7d3cbde82fec14cd3f300c11372de
Ayckinn <ayckinn@pm.me>
2021-12-07 22:02:04 +0100

    Add PLANNING DE LA JOURNEE folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_06/02_planning_journee/planning_journee.cpp
@@ -0,0 +1,30 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 06
+    EXERCICE  : 02 - PLANNING DE LA JOURNEE
+ * =================================================== */
+#include <iostream>
+#define LOOP(repeat) for(int i = 0; i < repeat; i++)
+
+int main()
+{
+   int pos_actuelle(0);
+   int nombre_villes(0);
+   int villes(0);
+   int total(0);
+
+   std::cin >> pos_actuelle >> nombre_villes;
+
+   LOOP(nombre_villes)
+   {
+      std::cin >> villes;
+
+      if(villes >= (pos_actuelle - 50) && villes <= (pos_actuelle + 50))
+         total++;
+   }
+   std::cout << total << std::endl;
+   return 0;
+}
