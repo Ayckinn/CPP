e046514ede7ef69dcb47eea6d1286da2fbca0efa
Ayckinn <ayckinn@pm.me>
2021-12-07 22:33:15 +0100

    Add TYPE D'ARBRES folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_06/05_type_arbres/README.md
@@ -0,0 +1,17 @@
+# TYPE D'ARBRES - C++ Version
+LEVEL 01 --> CHAPTER 06 --> EXERCICE 05
+
+---
+### **SCREENSHOT**
+
+<div align="center">
+    <img
+        src="https://github.com/Ayckinn/CPP/blob/main/FRANCE_IOI/LEVEL_01/Chapter_06/05_type_arbres/arbres.png"
+        alt="DEMO"
+        style="width:50%">
+</div>
+
+---
+### **RELEASE**
+
+- December 07' 2021
---
+++ FRANCE_IOI/LEVEL_01/Chapter_06/05_type_arbres/type_arbres.cpp
@@ -0,0 +1,33 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 06
+    EXERCICE  : 05 - TYPE D'ARBRES
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+   int height(0);
+   int folioles(0);
+
+   std::cin >> height >> folioles;
+
+   if(height <= 8)
+   {
+      if(folioles >= 8)
+         std::cout << "Tinuviel" << std::endl;
+      else
+         std::cout << "Falarion" << std::endl;
+   }
+   else if(height >= 10)
+   {
+      if(folioles >= 10)
+         std::cout << "Calaelen" << std::endl;
+      else
+         std::cout << "Dorthonion" << std::endl;
+   }
+   return 0;
+}
