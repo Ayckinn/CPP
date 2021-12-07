6d2d4ba81b993af4c455d029e4594cdbd9d8f5d9
Ayckinn <ayckinn@pm.me>
2021-12-07 21:54:15 +0100

    Add VILLES ET VILLAGES folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_06/01_villes_villages/villes_villages.cpp
@@ -0,0 +1,29 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 06
+    EXERCICE  : 01 - VILLES ET VILLAGES
+ * =================================================== */
+#include <iostream>
+#define LOOP(repeat) for(int i = 0; i < repeat; i++)
+
+int main()
+{
+   int ville(0);
+   int habitants(0);
+   int total(0);
+
+   std::cin >> ville;
+
+   LOOP(ville)
+   {
+      std::cin >> habitants;
+      
+      if(habitants > 10000)
+         total++;
+   }
+   std::cout << total << std::endl;
+   return 0;
+}
