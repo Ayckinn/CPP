1a20d75e2d75f8b80c113e77c4fce99262f18bda
Ayckinn <ayckinn@pm.me>
2021-12-08 21:45:58 +0100

    Add MAISON DE L'ESPION folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_07/02_maison_espion/maison_espion.cpp
@@ -0,0 +1,37 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 07
+    EXERCICE  : 02 - MAISON DE L'ESPION
+ * =================================================== */
+#include <iostream>
+#define LOOP(repeat) for(int i = 0; i < repeat; i++)
+
+int main()
+{
+   int xMin(0);
+   int xMax(0);
+   int yMin(0);
+   int yMax(0);
+   int maisons(0);
+   int xMaison(0);
+   int yMaison(0);
+   int zone(0);
+
+   std::cin >> xMin >> xMax >> yMin >> yMax >> maisons;
+
+   LOOP(maisons)
+   {
+      std::cin >> xMaison >> yMaison;
+
+      if((xMaison >= xMin) && (yMaison >= yMin))
+      {
+         if((xMaison <= xMax) && (yMaison <= yMax))
+            zone++;
+      }
+   }
+   std::cout << zone << std::endl;
+   return 0;
+}
