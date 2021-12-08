f6e84b2235a3eec5f1a90a477b748ae2da279124
Ayckinn <ayckinn@pm.me>
2021-12-08 22:00:52 +0100

    Add NB AMITIES ENTRE GARDES folder

---
+++ FRANCE_IOI/LEVEL_01/Chapter_07/04_amities_gardes/amities_garde.cpp
@@ -0,0 +1,26 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 07
+    EXERCICE  : 04 - AMITIES ENTRE GARDES
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+   int soldatADebut(0);
+   int soldatAFin(0);
+   int soldatBDebut(0);
+   int soldatBFin(0);
+
+   std::cin >> soldatADebut >> soldatAFin >> soldatBDebut >> soldatBFin;
+
+   if((soldatAFin < soldatBDebut) || (soldatADebut > soldatBFin))
+      std::cout << "Pas amis" << std::endl;
+   else
+      std::cout << "Amis" << std::endl;
+
+   return 0;
+}
