c1b561fd6a9c7118dfd89c7684f0dfd516c9b072
Ayckinn <ayckinn@pm.me>
2021-12-11 13:23:10 +0100

    Update main file

--- FRANCE_IOI/LEVEL_01/Chapter_01/02_presentation/presentation.cpp
+++ FRANCE_IOI/LEVEL_01/Chapter_01/02_presentation/presentation.cpp
@@ -4,17 +4,16 @@
     LANGUAGE  : C++
     LEVEL     : 01
     CHAPTER   : 01
-    EXERCICE  : 02 - PRESENTATION
+    EXERCICE  : 02
+    TITLE     : PRESENTATION
  * =================================================== */
-
 #include <iostream>
 
 int main()
 {
    	std::cout << "Coucou !\n";
    	std::cout << "Je m'appelle Camthalion\n";
-   	std::cout << "Ma devise est 'Parler peu mais parler bien'.";
+   	std::cout << "Ma devise est 'Parler peu mais parler bien'.\n";
 
-   	std::cout << std::endl;
    	return 0;
 }
