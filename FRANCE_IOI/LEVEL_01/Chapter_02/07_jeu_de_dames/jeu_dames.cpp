7234ccabbc186d785bb06769e231c80f9944a7a0
Ayckinn <ayckinn@pm.me>
2021-12-11 13:39:51 +0100

    Update main file

--- FRANCE_IOI/LEVEL_01/Chapter_02/07_jeu_de_dames/jeu_dames.cpp
+++ FRANCE_IOI/LEVEL_01/Chapter_02/07_jeu_de_dames/jeu_dames.cpp
@@ -4,22 +4,21 @@
     LANGUAGE  : C++
     LEVEL     : 01
     CHAPTER   : 02
-    EXERCICE  : 07 - JEU DE DAMES
+    EXERCICE  : 07
+    TITLE     : JEU DE DAMES
  * =================================================== */
-
 #include <iostream>
-
-#define loop(repeat) for(int i = 0; i < repeat; i++)
+#define LOOP(repeat) for(int i = 0; i < repeat; i++)
 
 int main()
 {
-    loop(20) 
+    LOOP(20) 
     {
-        loop(20)
+        LOOP(20)
             std::cout << "OX";
         std::cout << std::endl;
         
-        loop(20)
+        LOOP(20)
             std::cout << "XO";
         std::cout << std::endl;
     }
