ca8c3b1d96fbff1221a60e6c16359309223c1fd7
Ayckinn <ayckinn@pm.me>
2021-12-12 21:07:51 +0100

    Update main file

--- FRANCE_IOI/LEVEL_01/Chapter_03/06_partie_cache-cache/cache-cache.cpp
+++ FRANCE_IOI/LEVEL_01/Chapter_03/06_partie_cache-cache/cache-cache.cpp
@@ -4,15 +4,15 @@
     LANGUAGE  : C++
     LEVEL     : 01
     CHAPTER   : 03
-    EXERCICE  : 06 - UNE PARTIE DE CACHE-CACHE
+    EXERCICE  : 06
+    TITLE     : UNE PARTIE DE CACHE-CACHE
  * =================================================== */
 #include <iostream>
-
-#define loop(repeat) for(int i = 1; i < repeat; i++)
+#define LOOP(repeat) for(int i = 1; i < repeat; i++)
 
 int main()
 {
-    loop(101)
+    LOOP(101)
         std::cout << i << std::endl;
     
     std::cout << "J'arrive !" << std::endl;
