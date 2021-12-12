5275a321b09008a6cd3daa9f9b1d4a2b15994441
Ayckinn <ayckinn@pm.me>
2021-12-12 21:06:16 +0100

    Update main file

--- FRANCE_IOI/LEVEL_01/Chapter_03/05_cour_recreation/cour_recreation.cpp
+++ FRANCE_IOI/LEVEL_01/Chapter_03/05_cour_recreation/cour_recreation.cpp
@@ -4,7 +4,8 @@
     LANGUAGE  : C++
     LEVEL     : 01
     CHAPTER   : 03
-    EXERCICE  : 05 - COUR DE RECREATION
+    EXERCICE  : 05
+    TITLE     : COUR DE RECREATION
  * =================================================== */
 #include <iostream>
 
