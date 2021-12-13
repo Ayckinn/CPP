b1b01ad8501533346e62e8d9d0d637342dfbaae4
Ayckinn <ayckinn@pm.me>
2021-12-13 21:42:32 +0100

    Add CONVERSIONS DE DISTANCES folder

---
+++ FRANCE_IOI/LEVEL_02/Chapter_01/02_conversions_distances/conversions_distances.cpp
@@ -0,0 +1,21 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 02
+    CHAPTER   : 01
+    EXERCICE  : 02
+    TITLE     : CONVERSIONS DE DISTANCES
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+   float lieues = 0.707;
+   float kilometres;
+
+   std::cin >> kilometres;
+   std::cout << kilometres/lieues << std::endl;
+
+   return 0;
+}
