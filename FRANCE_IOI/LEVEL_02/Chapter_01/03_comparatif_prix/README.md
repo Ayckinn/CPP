34d3622befb47b47505280f4f7d6ef2e6a0ec9ab
Ayckinn <ayckinn@pm.me>
2021-12-14 22:45:58 +0100

    Add COMPARATIF DE PRIX folder

---
+++ FRANCE_IOI/LEVEL_02/Chapter_01/03_comparatif_prix/README.md
@@ -0,0 +1,17 @@
+# COMPARATIF DE PRIX - C++ Version
+LEVEL 02 --> CHAPTER 01 --> EXERCICE 03
+
+---
+### **SCREENSHOT**
+
+<div align="center">
+    <img
+        src="https://github.com/Ayckinn/CPP/blob/main/FRANCE_IOI/LEVEL_02/Chapter_01/03_comparatif_prix/comparatif.png"
+        alt="DEMO"
+        style="width:50%">
+</div>
+
+---
+### **RELEASE**
+
+- December 13' 2021
---
+++ FRANCE_IOI/LEVEL_02/Chapter_01/03_comparatif_prix/comparatif_prix.cpp
@@ -0,0 +1,33 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 02
+    CHAPTER   : 01
+    EXERCICE  : 03
+    TITLE     : COMPARATIF DE PRIX
+ * =================================================== */
+#include <iostream>
+#define LOOP(repeat) for(int i = 0; i < repeat; i++)
+
+int main()
+{
+    int vegetable(0);
+    std::cin >> vegetable;
+
+    LOOP(vegetable)
+    {
+        double weight(0);
+        double age_in_days(0);
+        double price(0);
+        
+        std::cin >> weight >> age_in_days >> price;
+        std::cout << (price / weight) << std::endl;
+
+        //-- DETAILED SOLUTION --//
+        // weight = (weight / age_in_days);
+        // price = (price / age_in_days / weight);
+        // std::cout << price << std::endl;
+    }
+    return 0;
+}
