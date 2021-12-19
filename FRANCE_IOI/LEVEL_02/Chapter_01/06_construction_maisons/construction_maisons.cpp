0a27faf95e6bee727642f2ff8f81af00bc9692ad
Ayckinn <ayckinn@pm.me>
2021-12-19 13:19:27 +0100

    Update files

---
+++ FRANCE_IOI/LEVEL_02/Chapter_01/06_construction_maisons/construction_maisons.cpp
@@ -0,0 +1,37 @@
+/*
+Pour la construction de votre nouvelle maison, 
+vous avez calculé la quantité de ciment nécessaire pour construire les fondations.
+De nature économe, vous souhaitez acheter exactement la quantité nécessaire mais malheureusement
+le magasin ne vend le ciment qu'en gros sacs. Vous souhaitez calculer combien tout cela va vous coûter.
+
+Ce que doit faire votre programme :
+Votre programme devra lire un nombre décimal,
+la quantité de ciment nécessaire pour les fondations de votre nouvelle maison, en kilos.
+Sachant que le ciment n'est vendu qu'en sacs de 60 kilos et que un sac coûte 45 euros,
+votre programme devra afficher le coût total du ciment.
+
+
+Exemple
+
+entrée :
+145.8
+
+sortie :
+135
+*/
+
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 02
+    CHAPTER   : 01
+    EXERCICE  : 06
+    TITLE     : CONSTRUCTION DE MAISONS
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+    return 0;
+}
