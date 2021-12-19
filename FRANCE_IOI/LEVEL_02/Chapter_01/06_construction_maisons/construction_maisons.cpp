da7e310e6f50206bae24c04fa6640a1442fc7c80
Ayckinn <ayckinn@pm.me>
2021-12-19 14:26:44 +0100

    Add SOIREE ORAGEUSE folder

--- FRANCE_IOI/LEVEL_02/Chapter_01/06_construction_maisons/construction_maisons.cpp
+++ FRANCE_IOI/LEVEL_02/Chapter_01/06_construction_maisons/construction_maisons.cpp
@@ -26,22 +26,27 @@ int main()
 
 /**
  * NOTE :
- * La fonction [CEIL] (CEILING = Plafond -> en haut) inclue dans la bibliothèque (CMATH)
+ * La fonction [CEIL](CEILING = Plafond -> en haut) inclue dans la bibliothèque (CMATH)
  * permet d'afficher un nombre décimal à l'arrondi supérieur. 
  * A l'inverse, la fonction [FLOOR](Sol -> en bas) permet d'afficher un nombre décimal à l'arrondi inférieur
+ * Et ce, quelque soit la valeur du nombre après la virgule.
+ * 
+ * Pour être au plus proche de l'arrondi en fonction de la valeur du nombre après la virgule,
+ * il faut utiliser la fonction [ROUND](Arrondi).
+ * Ainsi : 2,3 --> 2 et 3,8 --> 4
  * 
  * /!\ IL EST IMPORTANT DE BIEN MATRISER LES ARRONDIS, CELA PEUT CONSIDERABLEMENT CHANGER LE RESULTAT !!
  * 
  * Prenons l'exemple donné dans l'intitulé du cours :
  * 
  *  VERSION SANS ARRONDI
- *      (145.8 / 60) = 2.43
- *      (2.43 * 45)  = 109.35
- * Le coût total du ciment est de 109.35€
+ *      (145,8 / 60) = 2,43
+ *      (2,43 * 45)  = 109,35
+ * Le coût total du ciment est de 109,35€
  * 
  *  VERSION AVEC ARRONDI 
- *      145.8 --> 146
- *      (146 / 60) = 2.43 --> 3
+ *      145,8 --> 146
+ *      (146 / 60) = 2,43 --> 3
  *      (3 * 45)   = 135
  * Le coût total du ciment est de 135€
 **/
---
+++ FRANCE_IOI/LEVEL_02/Chapter_01/07_soiree_orageuse/soiree_orageuse.cpp
@@ -0,0 +1,15 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 02
+    CHAPTER   : 01
+    EXERCICE  : 07
+    TITLE     : SOIREE ORAGEUSE
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+    return 0;
+}
