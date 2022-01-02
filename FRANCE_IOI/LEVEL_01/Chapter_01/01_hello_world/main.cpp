861db06dc49de7f45068a4792eec691716512882
Ayckinn <ayckinn@pm.me>
2022-01-02 21:53:22 +0100

    Initial release

---
+++ FRANCE_IOI/LEVEL_01/Chapter_01/01_hello_world/main.cpp
@@ -0,0 +1,17 @@
+/* ============================== DESCRIPTION ============================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2021
+    LANGUAGE  : C++
+    LEVEL     : 01
+    CHAPTER   : 01
+    EXERCICE  : 01
+    TITLE     : HELLO WORLD !
+ * ========================================================================= */
+
+#include <iostream>
+
+int main()
+{
+  	std::cout << "Hello world!\n";
+  	return 0;
+}
