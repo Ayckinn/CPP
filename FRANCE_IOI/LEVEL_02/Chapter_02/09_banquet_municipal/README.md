643fb16143e8a858338d2eda9d8187a531b7c228
Ayckinn <ayckinn@pm.me>
2022-01-27 22:20:46 +0100

    Initial release

---
+++ FRANCE_IOI/LEVEL_02/Chapter_02/09_banquet_municipal/README.md
@@ -0,0 +1,17 @@
+# BANQUET MUNICIPAL - C++ Version
+LEVEL 02 --> CHAPTER 02 --> EXERCICE 09
+
+---
+### **SCREENSHOT**
+
+<div align="center">
+    <img
+        src="https://github.com/Ayckinn/CPP/blob/main/FRANCE_IOI/LEVEL_02/Chapter_02/09_banquet_municipal/todo.png"
+        alt="DEMO"
+        style="width:50%">
+</div>
+
+---
+### **RELEASE**
+
+- January 27th, 2022
---
+++ FRANCE_IOI/LEVEL_02/Chapter_02/09_banquet_municipal/main.cpp
@@ -0,0 +1,40 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2022
+    LANGUAGE  : C++
+    LEVEL     : 02
+    CHAPTER   : 02
+    EXERCICE  : 09
+    TITLE     : BANQUET MUNICIPAL
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+    int nb_pos = 0;
+    int nb_changes = 0;
+
+    std::cin >> nb_pos >> nb_changes;
+
+    int pos[nb_pos] = {0};
+    for(int id = 0; id < nb_pos; id++)
+        std::cin >> pos[id];
+
+    int pos1 = 0;
+    int pos2 = 0;
+    int invert_pos = 0;
+
+    for(int i = 0; i < nb_changes; i++)
+    {
+        std::cin >> pos1 >> pos2;
+
+        invert_pos = pos[pos1];
+        pos[pos1] = pos[pos2];
+        pos[pos2] = invert_pos;
+    }
+
+    for(int id = 0; id < nb_pos; id++)
+        std::cout << pos[id] << std::endl;
+
+    return 0;
+}
