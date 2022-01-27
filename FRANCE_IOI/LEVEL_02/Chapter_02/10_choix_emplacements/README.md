7c8272edd72fd94af2da06982292437288be57d7
Ayckinn <ayckinn@pm.me>
2022-01-27 23:08:58 +0100

    Initial release

---
+++ FRANCE_IOI/LEVEL_02/Chapter_02/10_choix_emplacements/README.md
@@ -0,0 +1,17 @@
+# CHOIX DES EMPLACEMENTS - C++ Version
+LEVEL 02 --> CHAPTER 02 --> EXERCICE 10
+
+---
+### **SCREENSHOT**
+
+<div align="center">
+    <img
+        src="https://github.com/Ayckinn/CPP/blob/main/FRANCE_IOI/LEVEL_02/Chapter_02/10_choix_emplacements/todo.png"
+        alt="DEMO"
+        style="width:50%">
+</div>
+
+---
+### **RELEASE**
+
+- January 27th, 2022
---
+++ FRANCE_IOI/LEVEL_02/Chapter_02/10_choix_emplacements/main.cpp
@@ -0,0 +1,29 @@
+/* =================== DESCRIPTION =================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2022
+    LANGUAGE  : C++
+    LEVEL     : 02
+    CHAPTER   : 02
+    EXERCICE  : 10
+    TITLE     : CHOIX DES EMPLACEMENTS
+ * =================================================== */
+#include <iostream>
+
+int main()
+{
+    int nb_places = 0;
+    std::cin >> nb_places;
+
+    int dealers[nb_places] = {0};
+    int dealer_place = 0;
+    for(int i = 0; i < nb_places; i++)
+    {
+        std::cin >> dealer_place;
+        dealers[dealer_place] = i;
+    }
+
+    for(int pos = 0; pos < nb_places; pos++)
+        std::cout << dealers[pos] << std::endl;
+
+    return 0;
+}
