24c891e91f37d5bf286ee918236d321f28dcecbc
Ayckinn <ayckinn@pm.me>
2022-02-09 22:33:41 +0100

    Initial release

---
+++ FRANCE_IOI/LEVEL_02/Chapter_03/10_inscription_etudiants/README.md
@@ -0,0 +1,17 @@
+# INSCRIPTION D'ETUDIANTS - C++ Version
+LEVEL 02 --> CHAPTER 03 --> EXERCICE 10
+
+---
+### **SCREENSHOT**
+
+<div align="center">
+    <img
+        src="https://github.com/Ayckinn/CPP/blob/main/FRANCE_IOI/LEVEL_02/Chapter_03/10_inscription_etudiants/todo.png"
+        alt="DEMO"
+        style="width:50%">
+</div>
+
+---
+### **RELEASE**
+
+- February 9', 2022
---
+++ FRANCE_IOI/LEVEL_02/Chapter_03/10_inscription_etudiants/main.cpp
@@ -0,0 +1,30 @@
+/* ==================== DESCRIPTION ==================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2022
+    LANGUAGE  : C++
+    LEVEL     : 02
+    CHAPTER   : 03
+    EXERCICE  : 10
+    TITLE     : INSCRIPTION D'ETUDIANTS
+ * ===================================================== */
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string student_name;
+    std::cin >> student_name;
+    char student_init = student_name[0];
+
+    
+    if(student_init < 'G')
+        std::cout << 1 << std::endl;
+
+    else if(student_init > 'P')
+        std::cout << 3 << std::endl;
+
+    else
+        std::cout << 2 << std::endl;
+
+    return 0;
+}
