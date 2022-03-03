60698e083786e4b7a256b41e988b3cddac828732
Ayckinn <ayckinn@pm.me>
2022-03-03 23:11:57 +0100

    Initial release

--- FRANCE_IOI/LEVEL_02/Chapter_03/11_ngms_sns_vlls/main.cpp
+++ FRANCE_IOI/LEVEL_02/Chapter_03/11_ngms_sns_vlls/main.cpp
@@ -12,5 +12,20 @@
 
 int main()
 {
+    std::string name;
+
+    for(int title_author = 0; title_author < 2; title_author++)
+    {
+        getline(std::cin, name);
+
+        for(int unsigned i = 0; i < name.length(); i++)
+        {
+            if(name[i] != 'A' && name[i] != 'E' && name[i] != 'I' && name[i] != 'O' && \
+                name[i] != 'U' && name[i] != 'Y' && name[i] != ' ')
+                
+                std::cout << name[i];
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
