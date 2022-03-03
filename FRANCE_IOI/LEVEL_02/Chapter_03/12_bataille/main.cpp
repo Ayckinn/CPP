14d4e3f5c7d5829266543ca020652ae8943bd63c
Ayckinn <ayckinn@pm.me>
2022-03-03 23:18:15 +0100

    Initial release

--- FRANCE_IOI/LEVEL_02/Chapter_03/12_bataille/main.cpp
+++ FRANCE_IOI/LEVEL_02/Chapter_03/12_bataille/main.cpp
@@ -4,28 +4,13 @@
     LANGUAGE  : C++
     LEVEL     : 02
     CHAPTER   : 03
-    EXERCICE  : 11
-    TITLE     : NGMS SNS VLLS
+    EXERCICE  : 12
+    TITLE     : LA BATAILL
  * ===================================================== */
 #include <iostream>
 #include <string>
 
 int main()
 {
-    std::string name;
-
-    for(int title_author = 0; title_author < 2; title_author++)
-    {
-        getline(std::cin, name);
-
-        for(int unsigned i = 0; i < name.length(); i++)
-        {
-            if(name[i] != 'A' && name[i] != 'E' && name[i] != 'I' && name[i] != 'O' && \
-                name[i] != 'U' && name[i] != 'Y' && name[i] != ' ')
-                
-                std::cout << name[i];
-        }
-        std::cout << std::endl;
-    }
     return 0;
 }
