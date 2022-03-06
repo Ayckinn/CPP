a8eaa352e61f32c97e7f5ecba183cef7b619ac55
Ayckinn <ayckinn@pm.me>
2022-03-06 11:19:50 +0100

    Initial release

--- FRANCE_IOI/LEVEL_02/Chapter_03/12_bataille/main.cpp
+++ FRANCE_IOI/LEVEL_02/Chapter_03/12_bataille/main.cpp
@@ -12,5 +12,26 @@
 
 int main()
 {
+    std::string player1, player2;
+    std::cin >> player1 >> player2;
+
+    int p1_len = player1.length();
+    int p2_len = player2.length();
+    int turn = 0;
+
+    while(turn < p1_len && turn < p2_len && player1[turn] == player2[turn])
+        turn++;
+
+    if(turn == p1_len && turn == p2_len)
+        std::cout << "=" << std::endl;
+
+    else if(turn == p2_len || (turn < p1_len && player1[turn] < player2[turn]))
+        std::cout << "1" << std::endl;
+
+    else
+        std::cout << "2" << std::endl;
+
+    std::cout << turn << std::endl;
+
     return 0;
 }
