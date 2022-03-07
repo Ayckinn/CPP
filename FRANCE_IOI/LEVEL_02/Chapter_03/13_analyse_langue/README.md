56a76cd63dd90fff0d1416ed806d059577fb091c
Ayckinn <ayckinn@pm.me>
2022-03-07 22:14:51 +0100

    Initial release

---
+++ FRANCE_IOI/LEVEL_02/Chapter_03/13_analyse_langue/README.md
@@ -0,0 +1,17 @@
+# ANALYSE D'UNE LANGUE - C++ Version
+LEVEL 02 --> CHAPTER 03 --> EXERCICE 13
+
+---
+### **SCREENSHOT**
+
+<div align="center">
+    <img
+        src="https://github.com/Ayckinn/CPP/blob/main/FRANCE_IOI/LEVEL_02/Chapter_03/13_analyse_langue/todo.png"
+        alt="DEMO"
+        style="width:50%">
+</div>
+
+---
+### **RELEASE**
+
+- March 7' 2022
---
+++ FRANCE_IOI/LEVEL_02/Chapter_03/13_analyse_langue/main.cpp
@@ -0,0 +1,35 @@
+/* ==================== DESCRIPTION ==================== *
+    AUTHOR    : Ayckinn
+    COPYRIGHT : ©2022
+    LANGUAGE  : C++
+    LEVEL     : 02
+    CHAPTER   : 03
+    EXERCICE  : 13
+    TITLE     : ANALYSE D'UNE LANGUE
+ * ===================================================== */
+#include <iostream>
+#include <string>
+
+int main()
+{
+    char letter;
+    std::cin >> letter;
+
+    int nb_lines = 0;
+    std::cin >> nb_lines;
+    std::cin.ignore();
+
+    std::string text;
+    int nb_appears = 0;
+
+    for(int line = 0; line < nb_lines; line++)
+    {
+        getline(std::cin, text);
+
+        for(unsigned int i = 0; i < text.length(); i++)
+            if(text[i] == letter)
+                nb_appears++;
+    }
+    std::cout << nb_appears << std::endl;
+    return 0;
+}
