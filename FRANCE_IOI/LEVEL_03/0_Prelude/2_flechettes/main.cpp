232e3b232d4fb6c865bf6dbadd289fa278e943d4
Ayckinn <ayckinn@pm.me>
2022-03-14 18:56:19 +0100

    Initial release

--- FRANCE_IOI/LEVEL_03/0_Prelude/2_flechettes/main.cpp
+++ FRANCE_IOI/LEVEL_03/0_Prelude/2_flechettes/main.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <algorithm>
 
+
 char alphabet(int line, int column, char size)
 {
    line = std::min(line, size - 1 - line);
@@ -34,62 +35,3 @@ int main()
    }
    return 0;
 }
-
-
-/*
-#include <iostream>
-#include <algorithm>
-using namespace std;
- 
-char trouveLettre(int iLig, int iCol, int taille)
-{
-   iLig = min(iLig, taille-1-iLig);
-   iCol = min(iCol, taille-1-iCol);
-   return 'a' + min(iLig, iCol);
-} 
-int main()
-{
-   int nbLettres;
-   cin >> nbLettres;
-   int taille = 2 * nbLettres - 1;
-   for(int iLig = 0; iLig < taille; iLig++)
-   {
-      for(int iCol = 0; iCol < taille; iCol++)
-         cout << trouveLettre(iLig, iCol, taille);
-      cout << endl;
-   }
-   return 0;
-}
-*/
-
-/*
-#include <iostream>
-#include <algorithm>
-using namespace std;
-  
-char lettres[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-  
-// Variante possible pour declarer la variable 'lettres' :
-char lettres[27] = "abcdefghijklmnopqrstuvwxyz";
-
-char trouveLettre(int iLig, int iCol, int taille)
-{
-   iLig = min(iLig, taille-1-iLig);
-   iCol = min(iCol, taille-1-iCol);
-   return lettres[ min(iLig, iCol) ];
-} 
- 
-int main()
-{
-   int nbLettres;
-   cin >> nbLettres;
-   int taille = 2 * nbLettres - 1;
-   for(int iLig = 0; iLig < taille; iLig++)
-   {
-      for(int iCol = 0; iCol < taille; iCol++)
-         cout << trouveLettre(iLig, iCol, taille);
-      cout << endl;
-   }
-   return 0;
-}
-*/
