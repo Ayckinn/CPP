dd24befeac9319ef8039325949d607612cf014c7
Ayckinn <ayckinn@pm.me>
2022-03-16 21:46:16 +0100

    Initial release

---
+++ FRANCE_IOI/LEVEL_03/0_Prelude/4_lissage_de_signal/README.md
@@ -0,0 +1,17 @@
+# LISSAGE DE SIGNAL - C++ Version
+### LVL 3 - PRELUDE
+
+---
+### **SCREENSHOT**
+
+<div align="center">
+    <img
+        src="https://github.com/Ayckinn/CPP/blob/main/FRANCE_IOI/LEVEL_03/0_Prelude/4_lissage_de_signal/todo.png"
+        alt="DEMO"
+        style="width:50%">
+</div>
+
+---
+### **RELEASE**
+
+- March 16' 2022
---
+++ FRANCE_IOI/LEVEL_03/0_Prelude/4_lissage_de_signal/main.cpp
@@ -0,0 +1,52 @@
+/* ==================== DESCRIPTION ==================== *
+        AUTHOR | Ayckinn
+     COPYRIGHT | ©2022
+      LANGUAGE | C++
+         LEVEL | 03
+       CHAPTER | PRELUDE
+      EXERCICE | 04
+         TITLE | LISSAGE DE SIGNAL
+ * ===================================================== */
+#include <iostream>
+#include <cmath>
+
+int nb_values = 0;
+int turn = 0;
+const int MAX_VALUES = 200;
+double values[MAX_VALUES];
+double temp[MAX_VALUES];
+double limit = 0;
+
+//---------------------------------------------------------
+bool Smoothing()
+{
+   for(int pos = 1; pos < nb_values; pos++)
+   {
+      if(std::abs(values[pos] - values[pos - 1]) > limit)
+         return false;
+   }
+   return true;
+}
+//---------------------------------------------------------
+int main()
+{
+   
+
+   std::cin >> nb_values >> limit;
+
+   for(int pos = 0; pos < nb_values; pos++)
+      std::cin >> values[pos];
+
+   while(!Smoothing())
+   {
+      turn++;
+
+      for(int pos = 1; pos < (nb_values - 1); pos++)
+         temp[pos] = ((values[pos - 1] + values[pos + 1]) / 2);
+
+      for(int pos = 1; pos < (nb_values - 1); pos++)
+         values[pos] = temp[pos];
+   }
+   std::cout << turn << std::endl;
+   return 0;
+}
