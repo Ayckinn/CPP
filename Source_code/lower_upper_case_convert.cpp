e4a663259d01d2dd409d5cba0ba41b3ea6f53225
Ayckinn <ayckinn@pm.me>
2022-04-04 21:38:06 +0200

    Add Lower-Upper-Case file

---
+++ Source_code/lower_upper_case_convert.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main()
+{
+	char low_case = 'a';
+	char up_case = 'L';
+	std::string full_upper = "AYCKINN";
+	std::string full_lower = "lisa";
+
+	std::cout << "Original char : " << low_case << " - To upper : " << (char)toupper(low_case) << "\n";
+	std::cout << "Original char : " << up_case << " - To lower : " << (char)tolower(up_case) << "\n";
+
+	std::cout << "\nOriginal string : " << full_upper << "\n" << "To lower : ";
+	for(int i = 0; i < full_upper.size(); i++)
+		std::cout << (char)tolower(full_upper[i]);
+
+	std::cout << "\n\nOriginal string : " << full_lower << "\n" << "To upper : ";
+	for(int i = 0; i < full_lower.size(); i++)
+		std::cout << (char)toupper(full_lower[i]);
+
+	return 0;
+}
