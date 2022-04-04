d6058b3258eb0f1db5ee8f6826280a241897165b
Ayckinn <ayckinn@pm.me>
2022-04-04 22:09:34 +0200

    Initial release

---
+++ FRANCE_IOI/LEVEL_03/3_Gestion_de_caracteres/3_majuscules/README.md
@@ -0,0 +1,16 @@
+# MAJUSCULES - C++ Version
+### LVL 3 - GESTION DE CARACTERES 
+
+### **SCREENSHOT**
+
+#### To Do
+<div align="center">
+    <img
+        src="https://github.com/Ayckinn/CPP/blob/main/FRANCE_IOI/LEVEL_03/3_Gestion_de_caracteres/3_majuscules/todo.png"
+        alt="DEMO"
+        style="width:50%">
+</div>
+
+### **RELEASE**
+
+- April 04' 2022
---
+++ FRANCE_IOI/LEVEL_03/3_Gestion_de_caracteres/3_majuscules/main.cpp
@@ -0,0 +1,23 @@
+/* ==================== DESCRIPTION ==================== *
+        AUTHOR | Ayckinn
+     COPYRIGHT | ©2022
+      LANGUAGE | C++
+         LEVEL | 03
+       CHAPTER | 03
+      EXERCICE | 03
+         TITLE | MAJUSCULES
+ * ===================================================== */
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main()
+{
+	std::string user_txt;
+	getline(std::cin, user_txt);
+
+	for(unsigned int c = 0; c < user_txt.size(); c++)
+		std::cout << (char)toupper(user_txt[c]);
+
+	return 0;
+}
