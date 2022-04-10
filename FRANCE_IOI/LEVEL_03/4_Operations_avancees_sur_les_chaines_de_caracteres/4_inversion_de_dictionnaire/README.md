e15221a9e2711072ea20e264ab1ed6858501b15e
Ayckinn <ayckinn@pm.me>
2022-04-10 12:25:15 +0200

    Initial release

---
+++ FRANCE_IOI/LEVEL_03/4_Operations_avancees_sur_les_chaines_de_caracteres/4_inversion_de_dictionnaire/README.md
@@ -0,0 +1,18 @@
+# INVERSION DE DICTIONNAIRE - C++ Version
+### LVL 4 - OPERATIONS AVANCEES SUR LES CHAINES DE CARACTERES 
+
+### **SCREENSHOT**
+
+---
+#### To Do
+<div align="center">
+    <img
+        src="https://github.com/Ayckinn/CPP/blob/main/FRANCE_IOI/LEVEL_03/4_Operations_avancees_sur_les_chaines_de_caracteres/4_inversion_de_dictionnaire/todo.png"
+        alt="DEMO"
+        style="width:50%">
+</div>
+
+---
+### **RELEASE**
+
+- April 10' 2022
---
+++ FRANCE_IOI/LEVEL_03/4_Operations_avancees_sur_les_chaines_de_caracteres/4_inversion_de_dictionnaire/main.cpp
@@ -0,0 +1,36 @@
+/* ==================== DESCRIPTION ==================== *
+        AUTHOR | Ayckinn
+     COPYRIGHT | ©2022
+      LANGUAGE | C++
+         LEVEL | 03
+       CHAPTER | 04
+      EXERCICE | 03
+         TITLE | INVERSION DE DICTIONNAIRE
+ * ===================================================== */
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+   std::ios::sync_with_stdio(false);  //-- Run the program faster
+
+   int nb_words = 0;
+   std::cin >> nb_words;
+
+   std::string dict[nb_words];
+   for(int word = 0; word < nb_words; word++)
+   {
+      std::string original, translated;
+      std::cin >> original >> translated;
+
+      dict[word] = translated + " " + original;
+   }
+   //-- Sort the dictionnary by the translated word
+   std::sort(dict, dict + nb_words);
+
+   for(int word = 0; word < nb_words; word++)
+      std::cout << dict[word] << "\n";
+
+   return 0;
+}
